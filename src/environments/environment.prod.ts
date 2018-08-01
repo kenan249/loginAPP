b6592cdd6c3911e4fa506bd555b9353f5a59e6f1
kenan249 <kenan249@gmail.com>
2018-07-31 22:56:41 -0500

    firebase enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAvkEatwjCx27b1FF4mcxOMxG-U4trypvk",
+    authDomain: "loginapp-2c3f4.firebaseapp.com",
+    databaseURL: "https://loginapp-2c3f4.firebaseio.com",
+    projectId: "loginapp-2c3f4",
+    storageBucket: "loginapp-2c3f4.appspot.com",
+    messagingSenderId: "948239563265"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAvkEatwjCx27b1FF4mcxOMxG-U4trypvk",
+    authDomain: "loginapp-2c3f4.firebaseapp.com",
+    databaseURL: "https://loginapp-2c3f4.firebaseio.com",
+    projectId: "loginapp-2c3f4",
+    storageBucket: "loginapp-2c3f4.appspot.com",
+    messagingSenderId: "948239563265"
+  }
 };
 
 /*
